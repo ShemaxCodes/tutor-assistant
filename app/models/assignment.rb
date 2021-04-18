9c4ab9338603b293367d3d4243305884980577bd
Jashema Panapa <jcpanapa@gmail.com>
2021-04-18 07:26:08 -0400

    =	modified:   app/controllers/assignments_controller.rb 	modified:   app/models/assignment.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,14 +2,6 @@ class Assignment < ActiveRecord::Base
     has_many :user_assignments
     has_many :users, through: :user_assignments#, source: :user_id
     
-    def assign_to_student
-        users = User.all 
-        keys_to_extract = [:id]
-        users.map do |w|
-        w.select { |k,_| keys_to_extract.include? k }
-        end
-    end
+    
 end 
 
-#student = User.all.each_with_index {|a, b| puts "#{a.id}"}
-#student.find
