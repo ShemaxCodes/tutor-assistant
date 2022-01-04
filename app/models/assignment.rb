f6c7fd228af7dcc0b053afb2d47c0c4448abc31b
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-01-04 17:06:40 -0500

    Update assignment.rb

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,7 @@
 class Assignment < ActiveRecord::Base
     has_many :user_assignments
     has_many :users, through: :user_assignments#, source: :user_id
-    has_one_attached :file, allow_nil: true
+    has_one_attached :file
    
     
     
